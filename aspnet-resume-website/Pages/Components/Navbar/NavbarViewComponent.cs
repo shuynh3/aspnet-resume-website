01806a33e55dec06a58c8022967d949e6d8ef469
shuynh3 <47999699+shuynh3@users.noreply.github.com>
2022-07-16 18:28:28 -0700

    Make the navbar a view component

---
+++ aspnet-resume-website/Pages/Components/Navbar/NavbarViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_resume_website.Pages.Components.Navbar
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        public NavbarViewComponent()
+        {
+
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View("Default");
+        }
+    }
+}
