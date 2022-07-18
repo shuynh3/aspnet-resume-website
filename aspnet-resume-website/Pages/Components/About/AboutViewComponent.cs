941c6c47bbf0f963cee5c812e46d3ab73a6982c9
shuynh3 <47999699+shuynh3@users.noreply.github.com>
2022-07-17 19:40:51 -0700

    Complete responsive about section

---
+++ aspnet-resume-website/Pages/Components/About/AboutViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_resume_website.Pages.Components.Skills
+{
+    public class AboutViewComponent : ViewComponent
+    {
+        public AboutViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("About");
+        }
+    }
+}
