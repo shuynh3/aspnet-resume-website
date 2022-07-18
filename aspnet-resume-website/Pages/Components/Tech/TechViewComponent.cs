ed80dde933152851cf9fadb2f57c43dfb5b671d2
shuynh3 <47999699+shuynh3@users.noreply.github.com>
2022-07-17 21:38:39 -0700

    Add social links

---
+++ aspnet-resume-website/Pages/Components/Tech/TechViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_resume_website.Pages.Components.Tech
+{
+    public class TechViewComponent : ViewComponent
+    {
+        public TechViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Tech");
+        }
+    }
+}
---
+++ aspnet-resume-website/Pages/Contact.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace aspnet_resume_website.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
