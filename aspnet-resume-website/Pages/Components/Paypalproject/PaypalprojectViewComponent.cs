5a5b58d86259bdba3ef88328556e2acb0137cf29
shuynh3 <47999699+shuynh3@users.noreply.github.com>
2022-07-20 23:59:23 -0700

    Finish projects section

---
+++ aspnet-resume-website/Pages/Components/Paypalproject/PaypalprojectViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_resume_website.Pages.Components.Paypalproject
+{
+    public class PaypalprojectViewComponent : ViewComponent
+    {
+        public PaypalprojectViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Paypalproject");
+        }
+    }
+}
---
+++ aspnet-resume-website/Pages/Components/Photographyproject/PhotographyprojectViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_resume_website.Pages.Components.Photographyproject
+{
+    public class PhotographyprojectViewComponent : ViewComponent
+    {
+        public PhotographyprojectViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View("Photographyproject");
+        }
+    }
+}
