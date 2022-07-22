8ce9f71617da239b8200f90413ea71cd6c273e95
shuynh3 <47999699+shuynh3@users.noreply.github.com>
2022-07-21 22:57:30 -0700

    Create dynamic view component for projects
     - Remove PaypalProject component
     - Remove PhotographyProject component
     - Create Project component
     - Create Project model
     - Allow invoking view component with parameters
     - Make Project generate custom css classes for each of the inputted image file names
     - Make Project generate unique id for image
     - Redo image switch javascript to work with any projects added

---
+++ aspnet-resume-website/Pages/Components/Project/ProjectModel.cs
@@ -0,0 +1,63 @@
+﻿namespace aspnet_resume_website.Pages.Components.Project
+{
+    public class ProjectModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string? GithubLink { get; set; }
+        public string? Link { get; set; }
+        public string ImageId { get; set; }
+        public string DefaultImageClass { get; set; }
+        public string ImageClassesString { get; set; }
+        public string ImageStyleClasses { get; set; }
+        public bool ImageCentered { get; set; }
+
+        public ProjectModel(string imageFileNames, string title, string description, string? githubLink, string? link, bool imageCentered)
+        {
+            var classNames = getClassNames(imageFileNames);
+
+            ImageId = classNames[0];
+            DefaultImageClass = classNames[0];
+            ImageClassesString = getClassesString(classNames);
+            ImageStyleClasses = getClassesStyles(imageFileNames, classNames);
+            Title = title;
+            Description = description;
+            GithubLink = githubLink;
+            Link = link;
+            ImageCentered = imageCentered;
+        }
+
+        private string[] getClassNames(string imageFileNames)
+        {
+            var uniqueSuffix = Guid.NewGuid().ToString();
+            var classNames = imageFileNames.Split(" ");
+            for (int i = 0; i < classNames.Length; i++)
+            {
+                classNames[0] += uniqueSuffix;
+            }
+            return classNames;
+        }
+
+        private string getClassesString(string[] classNames)
+        {
+            var classesString = "[";
+            for (int i = 0; i < classNames.Length; i++)
+            {
+                classesString += $"'{classNames[i]}', ";
+            }
+            return classesString + "]";
+        }
+
+        private string getClassesStyles(string imageFileNames, string[] classNames)
+        {
+            var fileNames = imageFileNames.Split(" ");
+            var imageStyles = "";
+            for (int i = 0; i < fileNames.Length; i++)
+            {
+                var style = $".{classNames[i]} {{ background-image: url(\"/images/{fileNames[i]}.png\"); }} ";
+                imageStyles += style;
+            }
+            return imageStyles;
+        }
+    }
+}
---
+++ aspnet-resume-website/Pages/Components/Project/ProjectViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_resume_website.Pages.Components.Project
+{
+    public class ProjectViewComponent : ViewComponent
+    {
+        public ProjectViewComponent() { }
+
+        public IViewComponentResult Invoke(string imageFileNames, string title, string description, string? githubLink = null, string? link = null, bool? imageCentered = false)
+        {
+            ProjectModel projectModel = new ProjectModel(imageFileNames, title, description, githubLink, link, (bool)imageCentered!);
+            return View("Project", projectModel);
+        }
+    }
+}
